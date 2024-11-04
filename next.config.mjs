a99909d69675d45dc327e2eb07936fdd260ddcbd
Rahul <rahul02dahiya@gmail.com>
2024-11-05 00:40:44 +0530

    git pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    // Set the basePath if your GitHub Pages site is hosted under a subpath
+    // Replace 'my-nextjs-site' with the name of your GitHub repository
+    basePath: '/studyHub',
+  };
+  
+  export default nextConfig;
+  
