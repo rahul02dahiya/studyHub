2a55ad05e8e5443537c02bf2641b299b1d25ff1b
Rahul <rahul02dahiya@gmail.com>
2024-11-05 22:27:56 +0530

    Revert "git pages deployment"
    This reverts commit a99909d69675d45dc327e2eb07936fdd260ddcbd.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    // Set the basePath if your GitHub Pages site is hosted under a subpath
-    // Replace 'my-nextjs-site' with the name of your GitHub repository
-    basePath: '/studyHub',
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
